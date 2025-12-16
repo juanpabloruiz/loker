b155d07af0d07cbd81164d9b4469c5773da2783b
juanpabloruiz <pabloruiz1980@gmail.com>
2025-12-16 10:21:34 -0300

    I add categories to read

--- admin/read.php
+++ admin/read.php
@@ -6,6 +6,26 @@ $stmt->execute();
 $result = $stmt->get_result();
 ?>
 
+
+<div class="row">
+    <div class="col-md-6">
+        <form method="POST" action="insertar_categoria.php" class="d-flex flex-column flex-md-row gap-2 mb-3">
+            <input type="text" name="nombre" class="form-control" placeholder="Categoría">
+            <input type="submit" value="Agregar categoría" class="btn btn-primary">
+        </form>
+        <?php
+        $sentencia = $conn->prepare("SELECT * FROM cats");
+        $sentencia->execute();
+        $resultado = $sentencia->get_result();
+        ?>
+        <ul>
+            <?php while ($row = $resultado->fetch_assoc()): ?>
+                <li><?= $row['name'] ?></li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
+</div>
+
 <table class="table table-hover">
     <tr>
         <th>Código</th>
